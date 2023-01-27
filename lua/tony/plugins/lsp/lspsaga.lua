51551fcdeb786c7a9a06f53e7cebd895a35ec707
czajkatony <4tonyc4@gmail.com>
2023-01-27 00:10:19 -0600

    add lsp support, finish config

---
+++ lua/tony/plugins/lsp/lspsaga.lua
@@ -0,0 +1,17 @@
+-- import lspsaga safely
+local saga_status, saga = pcall(require, "lspsaga")
+if not saga_status then
+  return
+end
+
+saga.setup({
+  -- keybinds for navigation in lspsaga window
+  move_in_saga = {prev = "<C-k>", next = "<C-j>"},
+  finder_action_keys = {open = "<CR>"},
+  scroll_preview = { scroll_down = "<C-f>", scroll_up = "<C-b>" },
+  -- use enter to open file with definition preview
+  definition = {
+    edit = "<CR>",
+  },
+})
+
--- lua/tony/plugins/lsp/mason.lua
+++ lua/tony/plugins/lsp/mason.lua
@@ -1,30 +1,67 @@
-require('mason').setup()
-require('mason-lspconfig').setup(
-{
--- list of servers for mason to install
-ensure_installed = {
-"tsserver",
-"html",
-"cssls",
-"tailwindcss",
-"sumneko_lua",
-"emmet_ls",
-"clangd",
-"cmake",
-"jsonls",
-"hls",
-"marksman",
-"pyright",
-"rust_analyzer",
-"solang",
-"sqls"
-},
-   -- auto-install configured servers (with lspconfig)
-   automatic_installation = true, -- not the same as ensure_installed
-}
+-- import mason plugin safely
+local mason_status, mason = pcall(require, "mason")
+if not mason_status then
+  return
+end
+
+-- import mason-lspconfig plugin safely
+local mason_lspconfig_status, mason_lspconfig = pcall(require, "mason-lspconfig")
+if not mason_lspconfig_status then
+  return
+end
+
+mason.setup()
+
+mason_lspconfig.setup({
+    ensure_installed = {
+     "tsserver",
+     "html",
+     "cssls",
+     "tailwindcss",
+     "sumneko_lua",
+     "emmet_ls",
+     "clangd",
+     "cmake",
+     "jsonls",
+     "marksman",
+     "pyright",
+     "rust_analyzer",
+     "solang",
+     "hls"
+    }
+})
 
 
-)
+
+
+
+-- require('mason').setup()
+-- require('mason-lspconfig').setup(
+-- {
+-- -- list of servers for mason to install
+-- ensure_installed = {
+-- "tsserver",
+-- "html",
+-- "cssls",
+-- "tailwindcss",
+-- "sumneko_lua",
+-- "emmet_ls",
+-- "clangd",
+-- "cmake",
+-- "jsonls",
+-- "hls",
+-- "marksman",
+-- "pyright",
+-- "rust_analyzer",
+-- "solang",
+-- "sqls"
+-- },
+--    -- auto-install configured servers (with lspconfig)
+--    automatic_installation = true, -- not the same as ensure_installed
+-- }
+--
+--
+-- )
 
 
 
