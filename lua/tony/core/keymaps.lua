403c1ad35a19effaad95b4267993980ecb825303
czajkatony <4tonyc4@gmail.com>
2023-02-02 14:14:13 -0600

    Added keybind for switching windows

--- lua/tony/core/keymaps.lua
+++ lua/tony/core/keymaps.lua
@@ -18,6 +18,8 @@ keymap.set("n", "<leader>tx", ":tabclose<CR>") -- close current tab
 keymap.set("n", "<leader>tn", ":tabn<CR>") --  go to next tab
 keymap.set("n", "<leader>tp", ":tabp<CR>") --  go to previous tabeymap.set()
 
+keymap.set("n", "<leader>wh", "<C-w>h")
+keymap.set("n", "<leader>wl", "<C-w>l")
 --Primeagen keymaps--
 
 --move while highlighted commands
